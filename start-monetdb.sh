e1202ccbe5a24d7e53493244885e9581aaa74462
R. Cijvat <robin.cijvat@monetdbsolutions.com>
2015-10-28 11:06:06 +0100

    Add q to dependency list, and replace polling code (that does not work in this case) with simple sleep statement that will hopefully always pass

--- start-monetdb.sh
+++ start-monetdb.sh
@@ -3,25 +3,7 @@
 monetdbd create ./dbfarm
 monetdbd start ./dbfarm
 
-function test_monetdb_connection() {
-  mclient -d blaeu -s 'SELECT 1' &> /dev/null
-  local status=$?
-  if [ $status -ne 0 ]; then
-    return 0
-  fi
-  return 1
-}
-
-for i in {30..0}; do
-  echo 'Testing MonetDB connection ' $i
-	if test_monetdb_connection ; then
-		echo 'Waiting for MonetDB to start...'
-  	sleep 1
-  else
-    echo 'MonetDB is running'
-    break
-	fi
-done
+sleep 10
 
 monetdb create test
 monetdb release test
