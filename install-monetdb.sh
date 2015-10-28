0e48e46c2d38565516ad3ff1e29d931ae4b35273
R. Cijvat <robin.cijvat@monetdbsolutions.com>
2015-10-28 07:19:43 +0100

    Fix travis

--- install-monetdb.sh
+++ install-monetdb.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository -s http://dev.monetdb.org/downloads/deb/
+add-apt-repository -h
+
+add-apt-repository "http://dev.monetdb.org/downloads/deb/ monetdb"
+
+cat /etc/apt/sources.list
 
 wget --output-document=- https://www.monetdb.org/downloads/MonetDB-GPG-KEY | apt-key add -
 
