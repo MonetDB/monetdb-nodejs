bf4b8ddb840985bec82183180166f20be3cbd2ca
R. Cijvat <robin.cijvat@monetdbsolutions.com>
2015-10-28 10:12:41 +0100

    Improve build process

--- start-monetdb.sh
+++ start-monetdb.sh
@@ -3,7 +3,25 @@
 monetdbd create ./dbfarm
 monetdbd start ./dbfarm
 
-sleep 10
+function test_monetdb_connection() {
+  mclient -d blaeu -s 'SELECT 1' &> /dev/null
+  local status=$?
+  if [ $status -ne 0 ]; then
+    return 0
+  fi
+  return 1
+}
+
+for i in {30..0}; do
+  echo 'Testing MonetDB connection ' $i
+	if test_monetdb_connection ; then
+		echo 'Waiting for MonetDB to start...'
+  	sleep 1
+  else
+    echo 'MonetDB is running'
+    break
+	fi
+done
 
 monetdb create test
 monetdb release test
