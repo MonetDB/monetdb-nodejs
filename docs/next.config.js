0379280746e27f6b322e73ce0a34660d50f5997a
svetlin <svetlin.stalinov@monetdbsolutions.com>
2023-11-02 12:10:54 +0100

    fix config

--- docs/next.config.js
+++ docs/next.config.js
@@ -4,12 +4,18 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.js',
 })
 
+const isProduction = process.env.NODE_ENV === "production";
+const assetPrefix = isProduction ? "/monetdb-nodejs" : "";
+
 const nextConfig = {
   output: "export",
   distDir: 'dist',
   images: {
     unoptimized: true,
-  }
+  },
+  trailingSlash: true,
+  assetPrefix,
+  basePath: assetPrefix,
 }
 
 module.exports = {
